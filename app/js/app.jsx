ab3457f012b8887094137e0b42e850cada52af89
Katie Levy <klevy@sandiego.edu>
2017-02-20 14:29:00 -0800

    adding firebase

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import Container from './container';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import * as firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB-sssQKmCJSaKWi2OOgpfpI6FLyQ4RSpg",
+  authDomain: "unimarket-8dac1.firebaseapp.com",
+  databaseURL: "https://unimarket-8dac1.firebaseio.com",
+  storageBucket: "unimarket-8dac1.appspot.com",
+  messagingSenderId: "1064318433149",
+};
+firebase.initializeApp(config);
+
 
 injectTapEventPlugin();
 
